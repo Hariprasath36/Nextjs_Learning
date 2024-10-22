f67fcfec3c5738ec40b68a5517ca6e642b846b05
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-22 11:31:33 +0530

    added public

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/ui/Navbar'
 import React from 'react'
 
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
     <>
-      Navbar
+      <Navbar />
       {children}
-      Footer
+      <Footer />
     </>
   )
 }
