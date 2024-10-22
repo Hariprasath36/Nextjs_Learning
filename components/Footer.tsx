049139f2475b6e374c7cccfe8954ddbf433145b1
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-22 13:00:49 +0530

    running 48:00

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import Link from 'next/link'
+
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-12 mx-md:flex-col'>
+      <p>Copyright © 2024 JS Mastery Pro | All Rights Reserved
+      </p>
+      <div className='flex gap-x-9'>
+        <Link href='/terms-of-use'>Terms & Conditions</Link>
+
+        <Link href='/privacy-policy'>Policy</Link>
+
+      </div>
+    </footer>
   )
 }
 
--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -7,11 +7,31 @@ const Navbar = () => {
     <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white'>
      <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16'>
       <Link href='/'>
-     <Image src="/json-logo.svg" alt="logo" 
+     <Image src="/jsm-logo.svg" alt="JSM logo" 
       width={50} height={50}/>
       </Link>
-     </div>
-      
+     
+      <Image 
+      src='/hamburger-menu.svg'
+      width={30}
+      height={30}
+      alt="Hamburger menu"
+      className="block md:hidden"
+      />
+      <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-10'>
+        <li className='body-text text-gradient_blue-purple !font-bold'>
+          <Link href='https://jsmastery.pro/next13' target='_blank'>
+          Next.js 13
+          </Link>
+        </li>
+        <li className='body-text !font-bold'>
+          <Link href='https://jsmastery.pro/next13' target='_blank'>
+          MasterClass
+          </Link>
+        </li>
+
+      </ul>
+      </div>
     </nav>
   )
 }
