2b73970fb812faa15346c914c8c686f6734eb4fc
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-22 13:11:17 +0530

    running 52:00

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import { Input } from "@/components/ui/input"
 
 const SearchForm = () => {
   return (
-    <form>
-        <label>
+    <form className='flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5'> 
+        <label className='flex-center relative w-full max-w-sxl'>
         <Image
         src="/magnifying-glass.svg"
         className='absolute left-8'
@@ -12,7 +13,11 @@ const SearchForm = () => {
         height={32}
         alt="Search icon"
         />
-    
+    <Input 
+    className='base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-placeholder:text-white-800'
+    type='text'
+    placeholder='Search'
+    />
     </label>
     </form>
   )
