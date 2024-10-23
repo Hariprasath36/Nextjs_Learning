af074dad252013baa302a0e3f76fc3c6b362c571
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-23 13:39:15 +0530

    error 1:43:00

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,8 +3,13 @@ import SearchForm from '@/components/SearchForm'
 import React from 'react'
 import { getResources, getResourcesPlaylist } from '@/sanity/actions'
 import ResourceCard from '@/components/ResourceCard'
+import Header from '@/components/Header'
 
 export const revalidate = 900;
+
+interface Props {
+  searchParams: { [key: string]: string | undefined }
+}
 
 
 const Page = async () => {
@@ -26,7 +31,8 @@ const Page = async () => {
     
     <Filters />
     <section className="flex-center mt-6 w-full flex-col sm:mt-20">
-      HEADER
+
+      <Header />
     <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
     {resources?.length > 0 ? (
               resources.map((resource: any) => (
