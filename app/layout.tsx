b94b5649e7112cfd97a50b49472fca9b27a9d214
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-24 09:50:08 +0530

    finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "HP Project",
   description: "Learning About Next.js",
+  other: {
+    'theme-color': '#0d1117',
+    "color-scheme": "dark only",
+    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "twitter:card": "summary_large_image",
+    "og:url": "jsmastery.pro",
+    "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "og:type": "website",
+  }
 };
 
 export default function RootLayout({
